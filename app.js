78cf7ddca6a66e8be18ed7d2d806cb072c39cb7f
Gayan Denzil <gayandenzil@gmail.com>
2024-06-02 08:39:30 +1000

    My second AWS commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('My First AWS Attempt!'));
+app.get('/', (req, res) => res.send('My First AWS Attempt 2!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
